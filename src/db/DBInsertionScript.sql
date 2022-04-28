aa56799c6da6ffce563726ae4535db77efda44c6
Justin5021 <jc.wk01@gmail.com>
2022-04-29 05:24:13 +0800

    Database Creation Script
    - Worked out what tables are necessary and wrote the script

---
+++ src/db/DBInsertionScript.sql
@@ -0,0 +1,9 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  Justin
+ * Created: Apr 29, 2022
+ */
+
