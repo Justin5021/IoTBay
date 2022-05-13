cf353714efdf3848152b01984c56aab85bad8c2b
JackyBahary <jackyrichiebahary@gmail.com>
2022-05-13 14:58:12 +1000

    Created Product and Supplier Model Classes

--- src/java/uts/isd/model/Product.java
+++ src/java/uts/isd/model/Product.java
@@ -4,10 +4,108 @@
  */
 package uts.isd.model;
 
+import java.io.Serializable;
+
 /**
  *
- * @author User
+ * @author Jacky Bahary 13997263
  */
-public class Product {
+public class Product implements Serializable{
+    private int productID, productSerialNum, productQuantity, supplierID;
+    private float productPrice;
+    private String productName, productCategory, productBrand, productImageURL;
     
+    public Product(
+            int productID,
+            int productSerialNum,
+            int productQuantity,
+            int supplierID,
+            float productPrice,
+            String productName,
+            String productCategory,
+            String productBrand,
+            String productImageURL){
+        this.productID = productID;
+        this.productSerialNum = productSerialNum;
+        this.productQuantity = productQuantity;
+        this.supplierID = supplierID;
+        this.productPrice = productPrice;
+        this.productName = productName;
+        this.productCategory = productCategory;
+        this.productBrand = productBrand;
+        this.productImageURL = productImageURL;
+    }
+    
+    public int getProductID() {
+        return productID;
+    }
+    
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+    
+    public int getProductSerialNum() {
+        return productSerialNum;
+    }
+    
+    public void setProductSerialNum(int productSerialNum) {
+        this.productSerialNum = productSerialNum;
+    }
+    
+    public int getProductQuantity() {
+        return productQuantity;
+    }
+    
+    public void setProductQuantity(int productQuantity) {
+        this.productQuantity = productQuantity;
+    }
+    
+    public int getSupplierID() {
+        return supplierID;
+    }
+    
+    public void setSupplierID(int supplierID) {
+        this.supplierID = supplierID;
+    }
+    
+    public float getProductPrice() {
+        return productPrice;
+    }
+    
+    public void setProductPrice(float productPrice) {
+        this.productPrice = productPrice;
+    }
+    
+    public String getProductName() {
+        return productName;
+    }
+    
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+    
+    public String getProductCategory() {
+        return productCategory;
+    }
+    
+    public void setProductCategory(String productCategory) {
+        this.productCategory = productCategory;
+    }
+    
+    public String getProductBrand() {
+        return productBrand;
+    }
+    
+    public void setProductBrand(String productBrand) {
+        this.productBrand = productBrand;
+    }
+    
+    public String getProductImageURL() {
+        return productImageURL;
+    }
+    
+    public void setProductImageURL(String productImageURL) {
+        this.productImageURL = productImageURL;
+    }
 }
+
--- src/java/uts/isd/model/Supplier.java
+++ src/java/uts/isd/model/Supplier.java
@@ -4,10 +4,46 @@
  */
 package uts.isd.model;
 
+import java.io.Serializable;
+
 /**
  *
- * @author User
+ * @author Jacky Bahary 13997263
  */
-public class Supplier {
+public class Supplier implements Serializable{
+    private int supplierID;
+    private String supplierAddress, supplierPhone;
     
+    public Supplier(
+            int supplierID,
+            String supplierAddress,
+            String supplierPhone){
+        this.supplierID = supplierID;
+        this.supplierAddress = supplierAddress;
+        this.supplierPhone = supplierPhone;
+    }
+    
+    public int getSupplierID() {
+        return supplierID;
+    }
+    
+    public void setSupplierID(int supplierID) {
+        this.supplierID = supplierID;
+    }
+    
+    public String getSupplierAddress() {
+        return supplierAddress;
+    }
+    
+    public void setSupplierAddress(String supplierAddress) {
+        this.supplierAddress = supplierAddress;
+    }
+    
+    public String getSupplierPhone() {
+        return supplierPhone;
+    }
+    
+    public void setSupplierPhone(String supplierPhone) {
+        this.supplierPhone = supplierPhone;
+    }
 }
